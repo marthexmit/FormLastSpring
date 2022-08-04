cbd5afc0211b4da3b465f20deb9a810dd70356bd
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-08-04 11:07:59 -0300

    Feat: Created Button micro component

---
+++ ui.frontend/src/components/Micro/Button/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :style="{ 'font-family': fontFamilyButton, 'background-color': backColorButton, 'color': colorFieldButton}">{{ Btext }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+  props: {
+    Btext: {
+      type: String
+    },
+    backColorButton: {
+      type: String
+    },
+    fontFamilyButton: {
+      type: String
+    },
+    colorFieldButton: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+</style>
