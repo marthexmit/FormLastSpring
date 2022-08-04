52e083d25c1856ff2dc0b7f59a421ce3f5115421
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-08-04 15:53:30 -0300

    feat: label component structure

---
+++ ui.frontend/src/components/Micro/Label/Label.vue
@@ -0,0 +1,31 @@
+<template>
+    <label
+        :for="ForInput"
+        :class="LabelClasses">
+        <slot>{{ LabelContent }}</slot>
+    </label>
+</template>
+
+<script>
+export default {
+    name: 'Label',
+    props: {
+        ForInput: {
+            type: String,
+            default: ''
+        },
+        LabelClasses: {
+            type: Array,
+            default: () => ['default']
+        },
+        LabelContent: {
+            type: String,
+            default: 'Label'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
