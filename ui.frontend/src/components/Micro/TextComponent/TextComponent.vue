0a08a2bc7d11edfba12f897a6713464d3434408a
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-08-04 11:02:01 -0300

    Feat: Created Text micro component

---
+++ ui.frontend/src/components/Micro/TextComponent/TextComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <p :style="{ 'font-family': fontFamilyText, 'color': colorFieldText,}">{{ newtext }}</p>
+</template>
+
+<script>
+export default {
+  name: 'TextComponent',
+  props: {
+    newtext: {
+      type: String
+    },
+    fontFamilyText: {
+      type: String
+    },
+    colorFieldText: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './TextComponent.scss'
+</style>
