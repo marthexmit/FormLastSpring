c593b135c9e8fb1c228e6123890aec58745a7215
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-08-16 09:49:20 -0300

    Refactor: adapting component to header

--- ui.frontend/src/components/Micro/Title/Title.vue
+++ ui.frontend/src/components/Micro/Title/Title.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <h1 v-if="type === 'h1'" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }}</h1>
-        <h2 v-else-if="type === 'h2'" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }}</h2>
-        <h3 v-else-if="type === 'h3'" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }}</h3>
-        <h4 v-else-if="type === 'h4'" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }}</h4>
-        <h5 v-else-if="type === 'h5'" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }}</h5>
-        <h6 v-else-if="type === 'h6'" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }}</h6>
+        <h1 v-if="type === 'h1'" :id = "smallTitle" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }} {{tabNumber}}</h1>
+        <h2 v-else-if="type === 'h2'" :id = "blueTitle" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }} {{tabNumber}}</h2>
+        <h3 v-else-if="type === 'h3'" :id = "blueTitle" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }} {{tabNumber}}</h3>
+        <h4 v-else-if="type === 'h4'" :id = "blueTitle" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }} {{tabNumber}}</h4>
+        <h5 v-else-if="type === 'h5'" :id = "blueTitle" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }} {{tabNumber}}</h5>
+        <h6 v-else-if="type === 'h6'" :id = "blueTitle" :style="{ 'font-family': fontFamily, 'color': colorField,}">{{ tMessage }} {{tabNumber}}</h6>
     </div>
 </template>
 
@@ -24,6 +24,15 @@ export default {
       type: String
     },
     colorField: {
+      type: String
+    },
+    tabNumber: {
+      type: String
+    },
+    blueTitle: {
+      type: String
+    },
+    smallTitle: {
       type: String
     }
   }
