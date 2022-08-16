9263af1a1673fa66cc5eeb078dfe1d74fc86cf41
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-08-16 10:31:33 -0300

    feat: form field has been created

---
+++ ui.frontend/src/components/FormFields/FormField.vue
@@ -0,0 +1,32 @@
+<template>
+  <FullName v-if="field.InputType === 'fullname'"
+            :fieldProps="field"
+  />
+  <Birthday v-else-if="field.InputType === 'birthday'"
+            :fieldProps="field"
+  />
+  <DefaultField v-else :fieldProps="field"/>
+</template>
+
+<script>
+import FullName from '@/components/FormFields/FullName/FullName.vue'
+import DefaultField from '@/components/FormFields/DefaultField/DefaultField.vue'
+import Birthday from '@/components/FormFields/Birthday/Birthday.vue'
+export default {
+  name: 'FormField',
+  components: {
+    FullName,
+    DefaultField,
+    Birthday
+  },
+  props: {
+    field: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
