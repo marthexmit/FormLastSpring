d7b4fd06fa3ca98e40a897235c3201e373167b99
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-08-16 10:49:38 -0300

    feat: social tab has been created

---
+++ ui.frontend/src/components/Tabs/Social/Social.vue
@@ -0,0 +1,31 @@
+<template>
+    <p>Social</p>
+</template>
+
+<script>
+export default {
+  name: 'Social',
+  props: {
+    fields: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.basic {
+    background-color: blue;
+}
+.main {
+    color: white;
+}
+.high {
+    font-size: 3em;
+}
+
+p {
+    text-align: center;
+}
+</style>
