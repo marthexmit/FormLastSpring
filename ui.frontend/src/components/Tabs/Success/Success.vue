24099122f4bf488a6d0da49be35c4ff92a9260d7
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-08-16 10:56:07 -0300

    Feat: Created success tab

---
+++ ui.frontend/src/components/Tabs/Success/Success.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="classes">
+        <p>Success</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Social',
+  props: {
+    classes: {
+      type: Array,
+      default: () => ['basic', 'main', 'high']
+    }
+  }
+}
+</script>
+
+<style scoped>
+.basic {
+    background-color: blue;
+}
+.main {
+    color: white;
+}
+.high {
+    font-size: 3em;
+}
+
+p {
+    text-align: center;
+}
+</style>
