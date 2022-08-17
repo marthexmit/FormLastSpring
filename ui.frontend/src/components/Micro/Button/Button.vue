4b04f82f2616b7a4eca7ff2718917a52bd223aa1
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-08-17 15:32:54 -0300

    Style: Styled to button of basic tab

--- ui.frontend/src/components/Micro/Button/Button.vue
+++ ui.frontend/src/components/Micro/Button/Button.vue
@@ -1,11 +1,14 @@
 <template>
-  <button :style="{ 'font-family': fontFamilyButton, 'background-color': backColorButton, 'color': colorFieldButton}" @click.prevent="clickButton">{{ Btext }}</button>
+  <button  class="BclassBasic"  v-if="type === '0'" :style="{ 'font-family': fontFamilyButton, 'background-color': backColorButton, 'color': colorFieldButton}" @click.prevent="clickButton">{{ Btext }} <img src="/content/dam/vue/VectorSetaDireita.png" alt="next image" /></button>
 </template>
 
 <script>
 export default {
   name: 'ButtonComponent',
   props: {
+    type: {
+      type: String
+    },
     Btext: {
       type: String
     },
@@ -26,5 +29,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import './Button.scss'
 </style>
