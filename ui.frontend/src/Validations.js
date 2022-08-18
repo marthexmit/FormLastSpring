34b88b5af34ce8068736d1b40ba849375786232b
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-08-18 08:58:09 -0300

    feat: basic validation implemented

---
+++ ui.frontend/src/Validations.js
@@ -0,0 +1,7 @@
+export default {
+  fullname: /^[A-Z]{1}[a-z]+( [a-zA-Z'"-]+)+$/,
+  nickname: /^\w+$/,
+  email: /^[^@\t\n\r ]+@[^@\t\n\r ]+\.[^@\t\n\r ]+$/,
+  phone: /^\d{11}$/,
+  birthday: /^\d{4}-\d{2}-\d{2}$/
+}
--- ui.frontend/src/components/Tabs/Basic/Basic.vue
+++ ui.frontend/src/components/Tabs/Basic/Basic.vue
@@ -53,7 +53,7 @@
     <Birthday />
     <fieldset class="form-footer">
       <Checkbox />
-      <ButtonComponent type="0" Btext="Next" :clickButton="nextTab" />
+      <ButtonComponent type="0" Btext="Next" :clickButton="validate" />
     </fieldset>
   </form>
 </template>
@@ -64,6 +64,7 @@ import Checkbox from '@/components/FormFields/Checkbox/Checkbox.vue'
 import Birthday from '@/components/FormFields/Birthday/Birthday.vue'
 import { mapActions } from 'vuex'
 import ButtonComponent from '@/components/Micro/Button/Button.vue'
+import Patterns from '@/Validations.js'
 export default {
   name: 'Basic',
   components: {
@@ -124,6 +125,44 @@ export default {
     ...mapActions(['nextTab']),
     setInputValue (field) {
       return localStorage.getItem(field) ? localStorage.getItem(field) : ''
+    },
+    validate () {
+      const invalids = ['fullname', 'nickname', 'email', 'phone'].filter((field) => {
+        const value = document.querySelector(`.${field} > input`).value
+        const error = document.querySelector(`.${field} > span`)
+        if (!Patterns[field].test(value)) {
+          if (field === 'nickname' || field === 'phone') {
+            if (value !== '') {
+              return field
+            } else {
+              error.style.visibility = 'hidden'
+            }
+          } else {
+            return field
+          }
+        } else {
+          error.style.visibility = 'hidden'
+        }
+      })
+
+      if (invalids.length) {
+        invalids.filter((field) => {
+          const error = document.querySelector(`.${field} > span`)
+          error.style.visibility = 'visible'
+        })
+      } else {
+        this.nextTab()
+      }
+
+      // const errors = ['fullname', 'nickname', 'email', 'phone'].forEach((field) => {
+      //   const value = document.querySelector(`.${field} > input`).value
+      //   const error = document.querySelector(`.${field} > span`)
+      //   if (!Patterns[field].test(value)) {
+      //     error.style.visibility = 'visible'
+      //   } else {
+      //     error.style.visibility = 'hidden'
+      //   }
+      // })
     }
   }
 }
